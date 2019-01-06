a9b66c97db6415543415054d2cc2db1c7fb9f10f
Mutlu Polatcan <mutlupolatcan@gmail.com>
2019-01-06 03:33:03 +0300

    colab-ssh setup is ready for usage

---
+++ colab-ssh.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PASSWORD=$1
+
+if [[ "$PASSWORD" == "" ]] 
+    then 
+        echo "[USAGE] ./colab-ssh.sh [password]"
+else
+    echo "Getting sshd config and apply settings..."
+    wget "https://raw.githubusercontent.com/mpolatcan/colab-ssh/master/sshd_config" > /dev/null
+    mv sshd_config /etc/ssh/
+    mkdir -p /var/run/sshd
+    /usr/sbin/sshd -D &
+    echo "sshd daemon is running..."
+    # Get .bashrc config
+    echo "Getting .bashrc config and apply settings..."
+    wget "https://raw.githubusercontent.com/mpolatcan/colab-ssh/master/.bashrc" > /dev/null
+    mv .bashrc /root/
+    echo ".basrhc updated!"
+    # Get Labstack Tunnel 
+    echo "Getting Labstack tunnel..."
+    wget "https://github.com/mpolatcan/colab-ssh/raw/master/tunnel-cli" > /dev/null
+    chmod +x tunnel-cli
+    # Setting password
+    echo "Setting password..."
+    echo root:$PASSWORD | chpasswd
+    # Tunnel is activating
+    echo "Tunnel is activating..."
+    ./tunnel-cli --tcp 22 &
+fi
+
--- connect-tunnel-ssh.sh
+++
@@ -1,6 +0,0 @@
-!chmod +x tunnel-cli
-!mkdir -p /var/run/sshd
-!echo root:0019030 | chpasswd
-!apt-get install htop nano pciutils net-tools vim
-get_ipython().system_raw('/usr/sbin/sshd -D &')
-!./tunnel-cli --tcp 22
