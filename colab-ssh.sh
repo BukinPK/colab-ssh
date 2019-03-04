62cd2dbc82bff42b95f58249641abd143a7b6377
Roman Troyan <bukinpk@gmail.com>
2019-03-04 18:15:19 +0200

    Update colab-ssh.sh

--- colab-ssh.sh
+++ colab-ssh.sh
@@ -1,48 +1,31 @@
 #!/bin/bash
 
 PASSWORD=$1
-basedir=$(dirname $(realpath $0))
+BASEDIR=$(dirname $(realpath $0))
+
 echo $basedir
 if [[ "${PASSWORD}" == "" ]]
     then
         echo "[USAGE] ./colab-ssh.sh [password]"
 else
     ################################## SSH SETTINGS ##############################################
-    echo "Getting sshd config and apply settings..."
-    if [[ ! -e "sshd_config" ]]
-        then
-            wget "https://raw.githubusercontent.com/mpolatcan/colab-ssh/master/sshd_config" -q
-    fi
-    cp sshd_config /etc/ssh/
+    cp $BASEDIR/sshd_config /etc/ssh/
     mkdir -p /var/run/sshd
     /usr/sbin/sshd -D &
     echo "sshd daemon is running..."
 
     ################################## SYSTEM SETTINGS ##############################################
-    echo "Getting .bashrc config and apply settings..."
-    if [[ ! -e ".bashrc" ]]
-        then
-            wget "https://raw.githubusercontent.com/mpolatcan/colab-ssh/master/.bashrc" -q
-    fi
-    cp .bashrc /root/
+    cp $BASEDIR/.bashrc /root/
     echo ".basrhc updated!"
     apt-get -y -qq install htop pciutils net-tools vim
     echo "vim, htop, pciutils, net-tools installed..."
-    echo "Setting password..."
     echo root:${PASSWORD} | chpasswd
 
     ################################## TUNNEL SETTINGS ##############################################
-    echo "Getting Labstack tunnel..."
-    if [[ ! -e "tunnel-cli" ]]
-        then
-            wget "https://github.com/mpolatcan/colab-ssh/raw/master/tunnel-cli" -q
-            chmod +x tunnel-cli
-    fi
     # Tunnel is activating
     echo "Tunnel is activating..."
     rm -rf ~/.tunnel.log
-    nohup ./tunnel-cli --tcp 22 >~/.tunnel.log 2>/dev/null &
+    nohup $BASEDIR/tunnel-cli --tcp 22 >~/.tunnel.log 2>/dev/null &
     sleep 1
     cat ~/.tunnel.log
 fi
-
