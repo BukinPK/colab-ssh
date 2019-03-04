e640dd9de18825d24cf28b20d7be47d8881f182d
Roman Troyan <bukinpk@gmail.com>
2019-03-04 17:01:25 +0200

    Update colab-ssh.sh

--- colab-ssh.sh
+++ colab-ssh.sh
@@ -10,7 +10,7 @@ else
     echo "Getting sshd config and apply settings..."
     if [[ ! -e "sshd_config" ]]
         then
-            wget "https://raw.githubusercontent.com/mpolatcan/colab-ssh/master/sshd_config"
+            wget "https://raw.githubusercontent.com/mpolatcan/colab-ssh/master/sshd_config" -q
     fi
     cp sshd_config /etc/ssh/
     mkdir -p /var/run/sshd
@@ -21,12 +21,12 @@ else
     echo "Getting .bashrc config and apply settings..."
     if [[ ! -e ".bashrc" ]]
         then
-            wget "https://raw.githubusercontent.com/mpolatcan/colab-ssh/master/.bashrc" > /dev/null
+            wget "https://raw.githubusercontent.com/mpolatcan/colab-ssh/master/.bashrc" -q
     fi
     cp .bashrc /root/
     echo ".basrhc updated!"
-    apt-get -y install nano htop pciutils net-tools vim tmux
-    echo "nano, htop, pciutils, net-tools installed..."
+    apt-get -y install htop pciutils net-tools vim -qq
+    echo "vim, htop, pciutils, net-tools installed..."
     echo "Setting password..."
     echo root:${PASSWORD} | chpasswd
 
@@ -34,7 +34,7 @@ else
     echo "Getting Labstack tunnel..."
     if [[ ! -e "tunnel-cli" ]]
         then
-            wget "https://github.com/mpolatcan/colab-ssh/raw/master/tunnel-cli"
+            wget "https://github.com/mpolatcan/colab-ssh/raw/master/tunnel-cli" -q
             chmod +x tunnel-cli
     fi
     # Tunnel is activating
